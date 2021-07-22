2676c9bedd05b3b7888cdd91fb4c36ba9eb4323b
mhchoi-2021 <mhchoi@ictis.kr>
2021-07-22 10:05:35 +0900

    210721

---
+++ test01_03.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(void)
+{
+    printf("천\n지\n인\n");
+    return 0;
+}
---
+++ test01_04.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("여보세요.\n안녕하세요.\n그럼 이만.\n");
+    return 0;
+}
