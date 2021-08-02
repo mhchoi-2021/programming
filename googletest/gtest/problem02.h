4795658d06e2ba813bf0a43cfa18e51d5acdf199
mhchoi-2021 <mhchoi@ictis.kr>
2021-08-03 01:15:10 +0900

    gtest

---
+++ googletest/gtest/problem02.h
@@ -0,0 +1,12 @@
+#ifndef _PROBLEM_H_
+#define _PROBLEM_H_
+
+
+int test01_01(int a, int b);
+int test01_02(int a, int b);
+char* test01_03();
+char* test01_04();
+char* test01_05();
+int test01_06();
+
+#endif
